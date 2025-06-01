f2c2dc3f08bdf5421a84f0f1813e3b3e835c0e5e
Yash <yashamanmeena2@gmail.com>
2025-06-01 14:31:14 +0530

    latest

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.VENDOR_API_URL || 'http://localhost:8000',
+  API_URL: 'https://mini-crm-req1.onrender.com' || 'http://localhost:8000',
   FRONTEND_URL: 'http://localhost:3000',
   GOOGLE_CLIENT_ID: '1027576728634-9bjvnhjqofc970kpoaegbjhf64a1sb3q.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: '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'
